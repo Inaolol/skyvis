03569f41ee25182ea49a18cc3b859d61520076ae
Olol <abdirisakmcm@gmail.com>
2024-07-21 22:08:48 +0300

    added

--- scripts/benchmark.py
+++ scripts/benchmark.py
@@ -3,4 +3,4 @@ from ultralytics.utils.benchmarks import benchmark
 # Benchmark on GPU
 if __name__ == '__main__':
     benchmark(model="C:/Users/abdir/Desktop/models/best.pt", 
-          data="coco8.yaml", imgsz=800, half=False, device=0)
+          data="C:/Users/abdir/Desktop/Tkno-13/data.yaml", imgsz=800, half=False, device='cpu')
